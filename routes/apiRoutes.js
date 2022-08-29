2f9d0b88e7b879e776112443ede02053b0d4e3af
Phinjock Sherpa <phinjock@gmail.com>
2022-08-28 21:59:55 -0700

    codes fixed to save in Json file WIP for files to display in notes.html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,8 +4,9 @@ const fs = require("fs");
 const uuid = require("../helpers/uuid");
 const path = require("path");
 const db = require("../db/db.json");
+const noteList = [];
 
-apiRoutes.get("/notes", (req, res) =>
+apiRoutes.get("/api/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
@@ -14,11 +15,15 @@ apiRoutes.get("/notes", (req, res) => {
   res.json(`${req.method} request received to get note`);
 });
 
+// GET Route for notes
+apiRoutes.get("/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
 apiRoutes.post("/notes", (req, res) => {
   res.json(`${req.method} request received to add a note`);
   const { title, text } = req.body;
 
-  // If all the required properties are present
   if (title && text) {
     // Variable for the object we will save
     const newNote = {
@@ -28,7 +33,7 @@ apiRoutes.post("/notes", (req, res) => {
     };
     // Convert the data to a string so we can save it
     const noteString = JSON.stringify(newNote);
-    // Write the string to a file
+    // Wrote the string to a file
     fs.writeFile(`./db/db.json`, noteString, (err) =>
       err
         ? console.error(err)
@@ -36,16 +41,34 @@ apiRoutes.post("/notes", (req, res) => {
             `Notes for ${newNote.title} has been written to JSON file`
           )
     );
-
+    noteList.push(newNote);
     const response = {
       status: "success",
-      body: newNote,
+      body: noteList,
     };
-
     console.log(response);
-    res.status(201).json(response);
   } else {
     res.status(500).json("Error in posting notes");
+  }
+});
+
+// POST Route for a new UX/UI note
+apiRoutes.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    fs.readFile(newTip, "./db/db.json");
+    const parsedData = JSON.parse(newTip);
+    parsedData.push(noteString);
+    res.json(`notes added successfully 🚀`);
+  } else {
+    res.error("Error in adding notes");
   }
 });
 
